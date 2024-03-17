542b1943e963ac9bdf71798a01d066bf4ec12e5a
Dzmitry Samuilik <dzmitry_samuilik@epam.com>
2024-03-17 16:24:24 +0200

    Task 2 (Serve SPA in AWS S3 and Cloudfront Services)

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            _My_Store_
           </Link>
         </Typography>
 
